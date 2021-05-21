ba7945d0bf8449cf494bf3b9029c2c583c864410
Kaja Limisiewicz <k.limisiewicz@gmail.com>
2021-05-21 02:58:38 +0200

    feat: overflow-hidden

--- src/components/TailwindContainer.js
+++ src/components/TailwindContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TailwindContainer = (props) => {
     return (
-        <div className="h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
+        <div className="h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12 overflow-hidden">
             <div className="relative py-3 mx-auto">
                 <div className="absolute inset-0 bg-gradient-to-r from-cyan-400 to-light-blue-500 shadow-lg transform skew-y-0 -rotate-12 rounded-3xl"></div>
                 <div className="relative bg-white shadow-lg rounded-3xl p-8">
